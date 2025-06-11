7c96ab3222a99bc82423d157d65c58fd32a88ce4
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-06-11 18:01:32 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -27,34 +27,16 @@
 //   )
 // }
 // export default App;
-
+import Comp1 from "./propsdelay/comp1";
 import { useState } from "react";
-import axios from "axios";
- const App=()=>{
-    const[input,setInput]=useState({})
-const handleInput=(e)=>{
-    let name=e.target.name;
-    let value=e.target.value;
 
-    setInput(values=>({...values,[name]:value}))
-    console.log(input)
-}
-    const handleSubmit=async()=>{
-        let api="http://localhost:3000/student";
-        const response=await axios.post(api,input )
-        alert("data inserted")
-      
-    }
-
+const Appp=()=>{
+  const[user,setUser]=useState("muskan");
   return(
     <>
-    <h1>application form</h1>
-    enter emp_num:<input type="text" name="emp_num" onChange={handleInput}/><br/><br/>
-    enter emp_name:<input type="text" name="emp_name" onChange={handleInput}/><br/><br/>
-    designation:<input type="text" name="designation" onChange={handleInput}/><br/><br/>
-    city:<input type="text" name="city" onChange={handleInput}/><br/><br/>
-    <button onClick={handleSubmit}>click here!!!</button><br/>
+    <h1>welcome:{user}!!!</h1>
+    <Comp1 user={user}/>
     </>
   )
 }
-export default App;
+export default Appp;
